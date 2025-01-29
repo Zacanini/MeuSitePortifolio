b3dc16cfa66ac02a1e09726205814ad9d2e7d1e9
Matheus De O Zacanini <zacanini2@gmail.com>
2025-01-29 13:27:31 -0300

    🐛 fix: corrige a rota da imagem para o caminho correto

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <NavBar/>
       <div id="home">
         <TextoUm />
-        <DownloadButton fileName="./public/images/CurrículoMatheusZacanini(2025-1).pdf" label="Baixar Curriculo" />
+        <DownloadButton fileName="dist/images/CurrículoMatheusZacanini(2025-1).pdf" label="Baixar Curriculo" />
         <BoxMidialLinks />
         <CaixaUm />
       </div>
