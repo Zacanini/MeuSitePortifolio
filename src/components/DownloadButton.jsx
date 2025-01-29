5361d63288766be0e5d479b865e981c879b1ccb2
Matheus De O Zacanini <zacanini2@gmail.com>
2025-01-29 14:01:29 -0300

    🚀 🐛 release: corrige a rota da imagem para o caminho correto (teste 3)

--- src/components/DownloadButton.jsx
+++ src/components/DownloadButton.jsx
@@ -3,7 +3,7 @@ import { FaFilePdf } from "react-icons/fa"; // Importa o ícone de PDF
 
 const DownloadButton = ({ fileName, label }) => {
     return (
-        <a href={"dist/images/CurrículoMatheusZacanini(2025-1).pdf"} download className="downloadButton">
+        <a href={"public/images/CurrículoMatheusZacanini(2025-1).pdf"} download className="downloadButton">
             <FaFilePdf style={{ marginRight: 8 }} /> {/* Ícone de PDF com um pequeno espaçamento */}
             {label}
         </a>
